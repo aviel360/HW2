c9ff211d0d64cc61262622a0e915a3776b6ab8b7
aviel360 <aviel3600@gmail.com>
2021-06-11 12:34:57 +0300

    stuck

--- SortedList/Node.cpp
+++ SortedList/Node.cpp
@@ -3,17 +3,18 @@ namespace mtm{
     class Node{
         T value;
         Node* next;
-
+        friend class SortedList;
+        friend class const_iterator;
     public:
         Node() = default;
         ~Node() = default;
         Node(T value, Node* next = nullptr);
         Node(const Node& node) = default;
         Node& operator=(const Node&);
+        bool operator !=(const Node&);
         Node& getNext() const;
         T& getValue() const;
         void setNext();
         void setValue();
-
     };
 }
--- SortedList/SortedList.h
+++ SortedList/SortedList.h
@@ -1,37 +1,86 @@
 #include <iostream>
+#include "Node.cpp"
 
 namespace mtm{
     typedef int T;
-//    template <class T>
+   // template <class T>
     class SortedList{
-        class Node;
-//       SortedList* next;
-        class Iterator;
+        class Node<T>* first;
 
     public:
         SortedList();
-        explicit SortedList(T& value);
         SortedList(const SortedList& list);
         ~SortedList();
         SortedList& operator=(const SortedList&);
         void insert(const T& value);
-        void remove(const T& value);
-        int getSize() const;
-        Iterator begin();
-        Iterator end();
+        void remove(const_iterator it);
+        class const_iterator;
+        const_iterator begin();
+        const_iterator end();
     };
 
-    class SortedList::Iterator{
-        const SortedList current;
-        int index;
-        Iterator(const SortedList* list, int index);
-        friend class SortedList;
+ //   template <class T>
+    class const_iterator{
+        class Node<T>* current;
+
+        const_iterator(const Node<T>& node);
+      //  friend class Node;
     
     public:
-        const int& operator*() const;
-        Iterator& operator++();
-        bool operator==(const Iterator& it) const;
-        Iterator& operator++(int);
-        bool operator!=(const Iterator& it) const;
+        const T& operator*() const;
+        const_iterator& operator++();
+        bool operator==(const const_iterator& it) const;
+        const_iterator& operator++(int);
     };
+
+    SortedList::SortedList(){ first = new Node<T>(); }
+
+    SortedList::SortedList(const SortedList& list) : first(){
+        Node<T>* current = list.first->getNext();
+        while(current != nullptr){
+            insert(current->getValue());
+            current = current->getNext();
+        }
+    }
+
+    void SortedList::insert(const T& value){
+        Node<T>* ptr = first->getNext();
+        while(ptr->getValue() < value && ptr->getNext() != nullptr){
+            ptr = ptr->getNext();
+        }
+        Node<T>* node = new Node<T>(value,ptr->getNext());
+        ptr->setNext(node);
+    }
+
+    SortedList& SortedList::operator=(const SortedList& list) {
+        if(this == &list) { return *this; }
+        SortedList::~SortedList();
+        first = list.first;
+    }
+
+    SortedList::~SortedList(){
+        Node<T>* current = first;
+        while(current != nullptr){
+            Node<T>* temp = current->getNext();
+            delete current;
+            current = temp;
+        }
+    }
+
+    const_iterator::const_iterator(const Node<T>& node) { *current = node; }
+
+    const T& const_iterator::operator*() const{ return current->getValue(); }
+
+    const_iterator& const_iterator::operator++(){
+        current = current->getNext();
+        return *this;
+    }
+
+    bool const_iterator::operator==(const const_iterator& it) const { return *(*this) == *it; }
+
+    const_iterator& const_iterator::operator++(int){
+        const_iterator& temp = *this;
+        current = current->getNext();
+        return temp;
+    }
 }
