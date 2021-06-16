626e33b4fdd398cbe8855ba4b040dde24804e381
aviel360 <aviel3600@gmail.com>
2021-06-16 14:14:19 +0300

    nop

--- Game/Soldier.h
+++ Game/Soldier.h
@@ -1,16 +1,24 @@
 #ifndef SOLDIER_H
 #define SOLDIER_H
 #include "Character.h"
+#include <cmath>
 
 namespace mtm{
     class Soldier : public Character {
-    
+
+        const int HEAD = ceil(range / 3 );
+        const int MAX_ATTACK_RANGE = HEAD*2 + 1;
+        const int AOE_DAMAGE = power / 2;
+        const int DAMAGE = ceil(power / 2);
+        static const int MAX_MOVEMENT = 3;
+        static const int AMMO = 3;
+        
     public:
-        bool move(const int distance) override;
+
+        void move(Board board, const GridPoint& src, const GridPoint& dst) override;
         void attack(Board board, const GridPoint & dst) override;
         void reload() override;
-        int* enemyInAoe(const GridPoint & dst);
-        static const int MAX_RANGE = 5;
+        void aoeAttack(Board board, const GridPoint & dst);
     };
 }
 
