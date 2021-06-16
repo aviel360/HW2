c468e529be739a3a67b4d2af2ae0e46510775d19
aviel360 <aviel3600@gmail.com>
2021-06-16 21:25:28 +0300

    Update Character.cpp

--- partB/Character.cpp
+++ partB/Character.cpp
@@ -1,1 +1,21 @@
 #include "Character.h"
+
+namespace mtm{
+    Character::Character(Team team, units_t health, units_t ammo, units_t range, units_t power) : 
+            team(team), health(health), ammo(ammo), range(range), power(power) {}
+    units_t Character::getAmmo(){
+        return ammo;
+    }
+    units_t Character::getRange(){
+        return range;
+    }
+    units_t Character::getHealth(){
+        return health;
+    }
+    units_t Character::getPower(){
+        return power;
+    }
+    units_t Character::getTeam(){
+        return team;
+    }
+}
