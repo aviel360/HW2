3ceaeb642c58868263a826ed91ea47cc7625505e
alonziv1 <82515685+alonziv1@users.noreply.github.com>
2021-06-22 12:38:41 +0300

    1

--- partB/Character.h
+++ partB/Character.h
@@ -22,21 +22,92 @@ namespace mtm{
         units_t power;
 
         public: 
+        /**
+         * @brief Construct a new Character object
+         * 
+         * @param team 
+         * @param health 
+         * @param ammo 
+         * @param range 
+         * @param power 
+         */
         Character(Team team, int health, int ammo, int range, int power);
         Character(const Character& character) = default;
         Character& operator=(const Character&) = default;
         virtual ~Character() = default;
+        /**
+         * @brief moves the character from the first point to the second 
+         * @param board - the game board 
+         * @param src - the current place of the character
+         * @param dst - the destination for the character
+         */
         virtual void move(Board& board, const GridPoint& src, const GridPoint& dst) = 0;
+        /**
+         * @brief make the character attack 
+         * 
+        * @param board - the game board 
+         * @param src - the current place of the character
+         * @param dst - the destination for the attack
+         */
         virtual void attack(Board& board, const GridPoint& src, const GridPoint& dst) = 0;
+        /**
+         * @brief - make the character reload
+         * 
+         */
         virtual void reload() = 0;
+        /**
+         * @brief Get the Symbol object
+         * 
+         * @return char represents the type and team 
+         */
         virtual char getSymbol() = 0;
+        /**
+         * @brief a "vuretual copy constructor" for copying the character deatils to a new character
+         * 
+         * @return std::shared_ptr<Character> 
+         */
         std::shared_ptr<Character> clone() const;
+        /**
+         * @brief Get the Ammo object
+         * 
+         * @return units_t 
+         */
         units_t getAmmo();
+        /**
+         * @brief Get the Range object
+         * 
+         * @return units_t 
+         */
         units_t getRange();
+        /**
+         * @brief Get the Health object
+         * 
+         * @return units_t 
+         */
         units_t getHealth();
+        /**
+         * @brief Get the Power object
+         * 
+         * @return units_t 
+         */
         units_t getPower();
+        /**
+         * @brief Get the Team object
+         * 
+         * @return units_t 
+         */
         units_t getTeam();
+        /**
+         * @brief lower the health of the character by this number
+         * 
+         * @param damage 
+         */
         void decreaseHealth(units_t damage);
+        /**
+         * @brief lower the health of the character by this number
+         * 
+         * @param heal 
+         */
         void increaseHealth(units_t heal);
     };
 
